c4cabba45bcfaea9b0f8ccd7fda8b0eb142d0d98
Vlad667 <genjas213@gmail.com>
2016-05-31 22:24:30 +0300

    Create Zav10.c

---
+++ Zav10.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+         int get_random_int_min_max(int min_rand, int max_rand)
+{
+          return min_rand + rand() % (max_rand - min_rand);
+}
+
+          int main()
+{
+	      int mas[100], i, n, max2 = 0;
+	      int min, max;
+	
+   	          printf ("Razmer massiva raven = ");
+	          scanf ("%d", &n);
+	
+	          printf ("\nMin rand raven = ");
+	          scanf ("%d", &min);
+	
+	          printf ("\nMax rand raven = ");
+	          scanf ("%d", &max);
+	
+	          srand(time(0));
+	
+	          for (i=0;i<n; i++){
+		      mas[i] = get_random_int_min_max(min, max);
+		      printf("\nmas[%d] = %d", i, mas[i]);
+}
+	
+	       for(i=0;i<n;i++){
+    		if(mas[i]>max2){
+			
+            max2 = mas[i];
+		}
+}
+	
+             printf ("\n\nMax znachennya ravno = %d", max2);
+	
+	         for(i=0;i<(n-1);i++){
+		     mas[i] = mas[i + 1];
+			 printf("\n\tmas[%d] = %d", i, mas[i]);
+	
+    }
+    
+    system("pause");
+	return 0;	
+}
