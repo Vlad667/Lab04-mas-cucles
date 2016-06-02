c7f8d9e3d708d3408ceabb5ecc58dc5ae5ac605d
Vlad667 <genjas213@gmail.com>
2016-06-02 16:15:05 +0300

    Create Zav11.c

---
+++ Zav11.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[]){
+
+    	int mas[20], para=0, i;
+	
+	    srand(time(0));
+	
+	    for(i=0;i<10;i++){
+		mas[i]=rand()%8;
+		printf("%d ", mas[i]);
+	
+}
+	
+	    for(i=0;i<8;i++){
+		if(mas[i]==mas[i+1])
+		para++;
+}
+	
+   
+    printf("\n\tV masive %d par odinakovih chisel = ", para);
+
+	system("pause");
+	
+    return 0;
+}
