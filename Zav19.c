b15931e6ca9e50f133f4866afcf011a3e2ddb510
Vlad667 <genjas213@gmail.com>
2016-06-02 16:52:31 +0300

    Create Zav19.c

---
+++ Zav19.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+
+{
+	int mas1[100], mas2[100], i, j, k = 0;
+	int flag, size;
+	
+	printf ("\nRazmer massiva = ");
+	scanf ("%d", &size);
+	
+	srand(time(0));
+	
+	for(i=0;i<size;i++)
+		mas1[i]=rand() % 20;
+	
+	for(i=0;i<size;i++) 
+		printf("\n\tmas[%2d] = %d", i, mas1[i]);
+	
+	for(i=0;i<size;i++) {
+		flag = 1;
+		for(j=0;j<size;j++) {
+			if(j==i)
+			continue;
+			if(mas1[i] == mas1[j]) {
+			flag = 0;
+			break;
+			}
+		}
+		if(flag) {
+		mas2[k] = mas1[i];
+		k++;
+	}
+	}
+	
+	printf("\nChiclo chto vstrechaetsy tolko odin raz = ");
+	for(i=0;i<k;i++) 
+	printf("\n\tmas[%2d] = %d",i,mas2[i]);
+	
+	system("pause");
+	
+    return 0;
+}
