dd9e04e2833dfe3d59a7a7e8a6325851139b2c4a
Vlad667 <genjas213@gmail.com>
2016-06-02 16:31:15 +0300

    Create Zav15.c

---
+++ Zav15.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+    int main()
+
+{
+	int mas[100], mas2[100], i, i2, n;
+	int min, max;
+	
+	printf ("\nRazmer massiva = ");
+	scanf ("%d", &n);
+
+	printf ("\nMin rand raven = ");
+	scanf ("%d", &min);
+	
+	printf ("\nMax rand raven = ");
+	scanf ("%d", &max);
+	
+	for(i=0,i2=1;i<n;i++,i2+= 2)
+		mas[i] = i2;
+	
+	for(i = 0; i < n; i++) 
+		printf("\nmas[%2d] = %d", i, mas[i]);
+	
+	for(i = 0, i2 = (n - 1); i < n; i++, i2--)
+		mas2[i] = mas[i2];
+		
+	for(i = 0; i < n; i++)
+		printf("\n\t\tmas2[%d] = %d", i, mas2[i]);
+	
+	system("pause");
+	return 0;
+}
