fb4c06a83fbb9d8ba6656b92e41c1eda6cd2863a
Vlad667 <genjas213@gmail.com>
2016-06-05 10:11:46 +0300

    Update Zav2.c

--- Zav2.c
+++ Zav2.c
@@ -8,21 +8,22 @@ int get_random_int_min_max(int min_rand, int max_rand)
 
     int main ()
 {
-	int mas[60], i, sum = 0;
+    int mas[60], i, sum = 0;
 	
-	for(i=0;i< 30;i++) 
+    for(i=0;i< 30;i++) 
     {
-		mas[i] = get_random_int_min_max(-30, 30);
-		printf("\nmas[%d] = %d", i, mas[i]);
-	}
+    mas[i] = get_random_int_min_max(-30, 30);
+    printf("\nmas[%d] = %d", i, mas[i]);
+    }
 	
-	    for(i = 0; i < 30; i++){
-	
-		if((mas[i] % 3) == 0){
+	    for(i = 0; i < 30; i++)
+        {
+	    if((mas[i] % 3) == 0){
 		sum += mas[i];
 		}
 	}
-		printf("\nsum = %d", sum);	
+	printf("\nsum = %d", sum);	
+    
     system("pause");
 	return 0;
 }
